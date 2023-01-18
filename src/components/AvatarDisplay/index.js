e10e6cdd809391cc795555e7bc88bfbf52e55d82
Raj Rai <rrai1590@gmail.com>
2023-01-18 17:40:40 +0530

    bug fixe -  initial render of Avatr on standalone page

--- src/components/AvatarDisplay/index.js
+++ src/components/AvatarDisplay/index.js
@@ -1,31 +1,23 @@
 import { PresentationControls, Stage, useGLTF } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import React, { Suspense, useEffect } from 'react'
-import avatar from '../../assets/img/avatar.glb'
+import React from 'react'
+import Model from '../Model'
 
-function Model(props) {
-  const { scene } = useGLTF(avatar)
-  return <primitive object={scene} {...props} />
-}
+// function Model(props) {
+//   const { scene } = useGLTF(avatar)
+//   return <primitive object={scene} {...props} />
+// }
 
 const AvatarDisplay = () => {
 
-  // Couldnt figure out why the avatar is scaling too large on initial render T-T. Hope to get the answer by joining you ; )
-  useEffect(()=> {
-    setTimeout(()=> {
-      alert(`If the avatar is too large or if there are any mistakes. It is requested to refresh the page. T-T`)  
-    }, 1000)
-  }, [])
   return (
-    <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }} style={{ position: 'absolute', background: '#000' }}>
-      <Suspense fallback={null}>
+    <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }} style={{ position: 'absolute', background: '#000', width: '100%', height: '100%' }}>
         <color attach={'background'} args={['#101010']} />
         <PresentationControls>
           <Stage environment={'dawn'}>
             <Model scale={0.5} />
           </Stage>
         </PresentationControls>
-      </Suspense>
     </Canvas>
   )
 }
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,56 +1,107 @@
-import React, { useEffect, useState } from 'react'
-import DragComponent from './DragComponent'
-import '../../index.css'
+import React, { useEffect, useState } from "react";
+import DragComponent from "./DragComponent";
+import "../../index.css";
 
 const Home = () => {
-  const [headerTime, setHeaderTime] = useState(``)
+  const [headerTime, setHeaderTime] = useState(``);
 
   useEffect(() => {
     setInterval(() => {
       var time = new Date();
       setHeaderTime(
-        ("0" + time.getHours()).slice(-2) + ":" +
-        ("0" + time.getMinutes()).slice(-2) + ":" +
-        ("0" + time.getSeconds()).slice(-2)
-      )
-    })
-  }, [])
+        ("0" + time.getHours()).slice(-2) +
+          ":" +
+          ("0" + time.getMinutes()).slice(-2) +
+          ":" +
+          ("0" + time.getSeconds()).slice(-2)
+      );
+    });
+  }, []);
+
+  const [containerClass, setContainerClass] = useState("center");
+  const changePosition = (position) => {
+    setContainerClass(position);
+  };
   return (
     <div>
       <header className="header">
         <div className="header--raio-btn">
           <div>
-            <input type="radio" name="position" id="top-right" value={'top-right'} />
-            <label className='radio-label' htmlFor='top-right'>Top-Right</label>
+            <input
+              type="radio"
+              name="position"
+              id="top-right"
+              value={"top-right"}
+              onClick={() => changePosition(`top-right`)}
+            />
+            <label className="radio-label" htmlFor="top-right">
+              Top-Right
+            </label>
           </div>
           <div>
-            <input type="radio" name="position" id="top-left" value={'top-left'} />
-            <label htmlFor='top-left' className='radio-label'>Top-Left</label>
+            <input
+              type="radio"
+              name="position"
+              id="top-left"
+              value={"top-left"}
+              onClick={() => changePosition(`top-left`)}
+            />
+            <label htmlFor="top-left" className="radio-label">
+              Top-Left
+            </label>
           </div>
           <div>
-            <input type="radio" name="position" id="bottom-right" value={'bottom-right'} />
-            <label htmlFor='bottom-right' className='radio-label'>Bottom-Right</label>
+            <input
+              type="radio"
+              name="position"
+              id="bottom-right"
+              value={"bottom-right"}
+              onClick={() => changePosition(`bottom-right`)}
+            />
+            <label htmlFor="bottom-right" className="radio-label">
+              Bottom-Right
+            </label>
           </div>
           <div>
-            <input type="radio" name="position" id="bottom-left" value={'bottom-left'} />
-            <label htmlFor='bottom-left' className='radio-label'>Bottom-left</label>
+            <input
+              type="radio"
+              name="position"
+              id="bottom-left"
+              value={"bottom-left"}
+              onClick={() => changePosition(`bottom-left`)}
+            />
+            <label htmlFor="bottom-left" className="radio-label">
+              Bottom-left
+            </label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="position"
+              id="center"
+              value={"center"}
+              onClick={() => changePosition(`center`)}
+            />
+            <label htmlFor="center" className="radio-label">
+              Center
+            </label>
           </div>
         </div>
 
-        <div className='header--timer'>
+        <div className="header--timer">
           <h3>{headerTime}</h3>
         </div>
       </header>
 
-      <div className='content-container'>
-        <DragComponent/>
+      <div className={`main-container ${containerClass}`}>
+          <DragComponent />
       </div>
 
-      <footer className='footer'>
-        <div className='footer--container'> Footer</div>
+      <footer className="footer">
+        <div className="footer--container"> Footer</div>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
